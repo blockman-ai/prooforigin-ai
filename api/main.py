56cb892d1d1e2093da362695c6a7f8210cd7d6b0
blockman-ai <grandblockmaster@gmail.com>
2026-05-28 10:35:41 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -278,3 +278,5 @@ def get_evidence(file_id: str):
         "file_id": file_id,
         "evidence": evidence,
     }
+    "bitcoinLiteAnchor": bitcoin_lite_anchor,
+"bitcoin_lite_anchor": bitcoin_lite_anchor,
